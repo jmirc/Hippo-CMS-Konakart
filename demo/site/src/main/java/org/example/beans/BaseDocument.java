bf4b161d48fb5a5738a035b5d2fe887bb13e2aa1
Jérôme Mirc <jerome.mirc@gmail.com>
2012-05-25 14:25:20 -0400

    delete demo

--- demo/site/src/main/java/org/example/beans/BaseDocument.java
+++
@@ -1,9 +0,0 @@
-package org.example.beans;
-
-import org.hippoecm.hst.content.beans.Node;
-import org.hippoecm.hst.content.beans.standard.HippoDocument;
-
-@Node(jcrType="myhippoproject:basedocument")
-public class BaseDocument extends HippoDocument {
-
-}
