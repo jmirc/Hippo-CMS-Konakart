a8499e3620d536e3aa6cbbfcc03c2d58d7a6c346
Jérôme Mirc <jerome.mirc@gmail.com>
2012-07-17 11:51:51 -0400

    Remove the KKProductServiceImpl from the spring xml

--- hippokart/site/src/main/java/org/onehippo/cms7/hst/hippokart/components/Home.java
+++ hippokart/site/src/main/java/org/onehippo/cms7/hst/hippokart/components/Home.java
@@ -1,10 +1,12 @@
 package org.onehippo.cms7.hst.hippokart.components;
 
+import com.konakart.al.KKAppException;
+import com.konakart.app.DataDescConstants;
+import com.konakart.app.KKException;
 import com.konakart.appif.ProductIf;
-import com.konakart.bl.ProductMgr;
+import com.konakart.al.ProductMgr;
 import org.hippoecm.hst.core.component.HstRequest;
 import org.onehippo.forge.konakart.hst.components.KKProductsOverview;
-import org.onehippo.forge.konakart.site.service.KKServiceHelper;
 
 import javax.annotation.Nonnull;
 
@@ -14,7 +16,15 @@ public class Home extends KKProductsOverview {
 
     @Override
     protected ProductIf[] searchProducts(@Nonnull HstRequest hstRequest) {
-        return KKServiceHelper.getKKProductService().
-                fetchNewProducts(hstRequest, ProductMgr.DONT_INCLUDE, true, 6);
+        try {
+            ProductMgr productMgr = getKKAppEng(hstRequest).getProductMgr();
+            productMgr.fetchNewProductsArray(NO_CATEGORY);
+            productMgr.orderCurrentProds(DataDescConstants.ORDER_BY_NAME_ASCENDING);
+            return productMgr.getCurrentProducts();
+        } catch (KKException e) {
+            return new ProductIf[0];
+        } catch (KKAppException e) {
+            return new ProductIf[0];
+        }
     }
 }
